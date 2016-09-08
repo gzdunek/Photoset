98dd6fa2bebcb1bac41552650c85825bd83f059c
Grzegorz Zdunek <rotcore@outlook.com>
2016-09-08 17:25:48 +0200

    Initial commit

---
+++ src/main/java/com/example/PhotosetApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosetApplication.class, args);
+	}
+}
